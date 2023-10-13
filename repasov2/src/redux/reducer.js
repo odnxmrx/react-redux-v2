289d2a5986fc5bb82502e8788629f5df6e61f9be
odnxmrx <armandopinedag@outlook.com>
2023-10-12 17:03:07 -0700

    add initial configuration

---
+++ repasov2/src/redux/reducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+    allCharacters: [],
+    allFavorites: [],
+    myFavorites: []   
+}
+
+const reducer = (state= initialState, action) => {
+    switch (action.type) {
+        // case value:
+            
+        //     break;
+        default:
+            return {
+                ...state
+            }
+    }
+}
+
+export default reducer;
